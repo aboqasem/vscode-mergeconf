5156031b1f2f4ec067afdf599e610697c8244f5a
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-03-26 19:16:25 +0800

    Rename extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { mergeThis } from './commands';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('MergeConf is now active!');
+  console.log('Merge Configurations is now active!');
 
   context.subscriptions.push(vscode.commands.registerCommand('mergeconf.mergeThis', mergeThis));
 }
