1dadf31cf803e332faaaf8a9c3696073837da72e
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-03-30 01:16:10 +0800

    Disable webview restoration after restart

--- src/extension.ts
+++ src/extension.ts
@@ -9,16 +9,6 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.commands.registerCommand('mergeconf.merge', merge.bind(undefined, context)));
 
   context.subscriptions.push(vscode.commands.registerCommand('mergeconf.mergeThis', mergeThis));
-
-  if (vscode.window.registerWebviewPanelSerializer) {
-    // Make sure we register a serializer in activation event
-    vscode.window.registerWebviewPanelSerializer(MergePanel.viewType, {
-      async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, _: any) {
-        webviewPanel.webview.options = { enableScripts: true };
-        MergePanel.revive(webviewPanel, context.extensionUri);
-      },
-    });
-  }
 }
 
 export function deactivate() {}
